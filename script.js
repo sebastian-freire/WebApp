f24bf38a142fac9a2e0bab58d2558f17309efeea
ShadowGod001 <55005167+ShadowGod001@users.noreply.github.com>
2025-04-18 02:49:50 -0300

    Ejercicio calculadora
    Buenas, esta es mi version del ejercicio. La lógica para las operaciones es distinta y creo que más clara de entender. El resto es muy parecido puesto que use sus mismos estilos y formato para que quedara igual al de ustedes.
    
    También tiene algunas mejoras como un botón de reinicio que borra el historial y el contador de respuestas correctas de la sesion no se pisa con el historial por lo que estan los 2 al mismo tiempo.

--- script.js
+++ script.js
@@ -1,159 +1,147 @@
-const operation = document.getElementById("operation");
+/*
+Se desea implementar una aplicación web para hacer ejercicios de cálculo sencillos.
+La página debe definir una expresión aritmética simple y aleatoria, e.g. 32 + 179.
+Se le pregunta al usuario cuál es la evaluación de dicha expresión.
+Se muestran opciones de respuesta aleatorias, en forma de botones.
+Cuando el usuario presiona uno de esos botones, se le muestra al usuario si respondió bien o mal.
+Se muestra otra pregunta aleatoria, y así sucesivamente.
+*/
 
-const button1 = document.getElementById("option1");
-const button2 = document.getElementById("option2");
-const button3 = document.getElementById("option3");
-const button4 = document.getElementById("option4");
+// Para instalar el servidor npm install json-server
+// Para correr el servidor json-server --watch db.json --port 3000 
 
-const respuesta = document.getElementById("respuesta");
-const contador = document.getElementById("contador");
+const buttons = document.querySelectorAll("#contenido-principal .btn"); // Selecciona todos los botones dentro del contenedor principal para que no agarre el de borrar historial
+const p1 = document.querySelector("#pregunta"); 
+const p2 = document.querySelector("#respuesta");
+let score = 0; // Variable para llevar el puntaje
 
-const contenedor = document.getElementById("contenedor4");
+function generarPregunta() {
 
-let objetoJson = {
-  cuenta: "",
-  resultado: "",
-  color: ""
-};
+    const primerNumero = Math.floor(Math.random() * 100) + 1;
+    const segundoNumero = Math.floor(Math.random() * 100) + 1;
+    const operador = ["+", "-", "*", "/"];
+    const randomOperador = operador[Math.floor(Math.random() * operador.length)];
 
-document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener("click", (event) => {
-    console.log(`Hiciste clic en el botón ${button.textContent} ${button.id}`);
-    objetoJson.resultado = button.textContent;
+    const cuenta = eval(`${primerNumero} ${randomOperador} ${segundoNumero}`); //eval evalua una cadena como si fuera código JS
+    const cuentaRedondeada = redondearResultado(cuenta);
 
-    if (button.textContent == respuestaCorrecta) {
-      bool = true;
-      objetoJson.color = "Green";
-    } else {
-      bool = false;
-      objetoJson.color = "Red";
-    }
 
-    agregarAJson(objetoJson);
-    respuestaBoton(bool);
-  });
-});
+    p1.textContent = `¿Cuánto es ${primerNumero} ${randomOperador} ${segundoNumero}?`;
 
-let respuestaCorrecta = 0;
-let contadorRespuestas = 0;
+    // Elegir 1 botón aleatorio para la respuesta correcta
+    const correctIndex = Math.floor(Math.random() * buttons.length);
+    buttons[correctIndex].textContent = cuentaRedondeada;
 
-function main() {
-  let num1 = numeroAleatorio();
-  let num2 = numeroAleatorio();
-  let operador = "";
-  let arrayValores = [];
+    // Lleno el resto de los botones con números random diferentes
+    for (let i=0; i<buttons.length; i++){
+        if (i !== correctIndex) {
+            let randomNumber;
+            do {
+                randomNumber = Math.floor(Math.random() * 200) + 1; // Número aleatorio entre 1 y 200
+            } while (randomNumber === cuentaRedondeada); // Asegurarse de que no sea el mismo número
+            buttons[i].textContent = randomNumber;
+        }
+        // Verificar que la respuesta sea correcta
+        buttons[i].onclick = () => {
+            const valor = parseFloat(buttons[i].textContent);
+            if (valor === cuentaRedondeada) {
+              p2.textContent = "✅Correcto";
+              score++; // Incrementar el puntaje
+            } else {
+              p2.textContent = "❌Intenta de nuevo.";
+            }
 
-  switch (operadorAleatorio()) {
-    case 0: //suma
-      respuestaCorrecta = num1 + num2;
-      operador = "+";
-      break;
-    case 1: //resta
-      respuestaCorrecta = num1 - num2;
-      operador = "-";
-      break;
-    case 2: //division
-      if (num1 < num2) {
-        [num1, num2] = [num2, num1];
-      }
-      respuestaCorrecta = num1 / num2;
-      operador = "/";
-      break;
-    case 3: //multiplicacion
-      respuestaCorrecta = num1 * num2;
-      operador = "x";
-      break;
+            // Guardar el resultado en el servidor
+            const resultado = {
+              cuenta: `${primerNumero} ${randomOperador} ${segundoNumero}`,
+              respuesta: valor,
+              color: valor === cuentaRedondeada ? 'Green' : 'Red'
+            };
+            
+            agregarResultado(resultado).then(mostrarResultados);
+
+            // Actualizar el puntaje en la interfaz
+            document.querySelector("#puntaje").textContent =
+            `Cantidad de respuestas correctas de la sesión: ${score}`;
+            
+      
+            // Nueva pregunta en cada clic
+            setTimeout(() => {
+              p2.textContent = ""; // Limpiar mensaje anterior
+              generarPregunta();
+            }, 1000); // Sleep de 1 seg para que se pueda ver el resultado
+          };        
+    };
+}
+
+// Redondea si es float, si no lo deja como está
+function redondearResultado(num) {
+  if (Number.isInteger(num)) {
+    return num;
+  } else {
+    return Math.floor(num * 100) / 100; // 2 decimales
   }
-
-  objetoJson.cuenta = `${num1} ${operador} ${num2}`;
-
-  respuestaCorrecta = Math.floor(respuestaCorrecta);
-  arrayValores.push(respuestaCorrecta);
-  arrayValores.push(generarNumeroSimilar(respuestaCorrecta, 25, arrayValores));
-  arrayValores.push(generarNumeroSimilar(respuestaCorrecta, 25, arrayValores));
-  arrayValores.push(generarNumeroSimilar(respuestaCorrecta, 25, arrayValores));
-
-  let nuevoArray = seleccionarElementosAleatorios(
-    arrayValores,
-    arrayValores.length
-  );
-  console.log(respuestaCorrecta);
-  console.log(nuevoArray);
-  operation.innerHTML = `${num1} <span> ${operador} </span> ${num2}:`;
-  button1.textContent = nuevoArray[0];
-  button2.textContent = nuevoArray[1];
-  button3.textContent = nuevoArray[2];
-  button4.textContent = nuevoArray[3];
-
-  leerDatos();
 }
 
-function generarNumeroSimilar(base, variacion, array) {
-  let numDev = Math.floor(base + (Math.random() * (variacion * 2) - variacion));
-  for (elemento of array) {
-    if (numDev === elemento) {
-      return generarNumeroSimilar(base, variacion, array);
-    }
-  }
-  return numDev;
-}
+// Lógica para eliminar el historial de resultados
+document.getElementById("btn-reiniciar").addEventListener("click", reiniciarResultados);
 
-function numeroAleatorio() {
-  return Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
-}
-
-function seleccionarElementosAleatorios(arr, cantidad) {
-  return arr.sort(() => Math.random() - 0.5).slice(0, cantidad);
-}
-
-function operadorAleatorio() {
-  const num = Math.floor(Math.random() * 4);
-  return num;
-}
-
-function respuestaBoton(resultado) {
-  main();
-
-  if (resultado) {
-    contadorRespuestas++;
-    contador.textContent = `Cantidad de respuestas correctas de la sesión: ${contadorRespuestas}`;
-    return (respuesta.textContent = "Respuesta Correcta ✅");
-  }
-  return (respuesta.textContent = "Respuesta Incorrecta ❌");
-}
-
-function leerDatos() {
-  let datos = JSON.parse(localStorage.getItem("datos")) || [];
-
-  contenedor.innerHTML = "";
-
-  datos
-    .slice()
-    .reverse()
-    .forEach((dato) => {
-      const div = document.createElement("div");
-      if (dato.color === "Green") {
-        div.className = "contenedor verde";
-      } else {
-        div.className = "contenedor rojo";
-      }
-      div.textContent = `${dato.cuenta} = ${dato.resultado}`;
-      contenedor.appendChild(div);
+// POST al servidor para agregar un resultado
+async function agregarResultado(resultado) {
+  try {
+    await fetch('http://localhost:3000/resultados', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(resultado)
     });
+  } catch (error) {
+    console.error('Error al agregar resultado:', error);
+  }
 }
 
-function agregarAJson(objeto) {
-  let datos = JSON.parse(localStorage.getItem("datos")) || [];
+// GET al servidor para obtener los resultados
+async function mostrarResultados() {
+  try {
+    const response = await fetch('http://localhost:3000/resultados');
+    const resultados = await response.json();
 
-  console.log(datos);
+    const contenedorHistorial = document.getElementById('contenedor-historial');
+    contenedorHistorial.innerHTML = '';
 
-  datos.push(objeto);
-
-  localStorage.setItem("datos", JSON.stringify(datos));
+    // Da vuelta los resultados para que el último resultado esté arriba
+    resultados.reverse().forEach((resultado) => {
+      const div = document.createElement('div'); // Crear un nuevo div para cada resultado
+      div.className = resultado.color === 'Green' ? 'contenedor verde' : 'contenedor rojo'; // Cambia el color del contenedor según el resultado con las clases CSS contenedor y rojo/verde
+      div.textContent = `${resultado.cuenta} = ${resultado.respuesta}`; // Muestra la cuenta y la respuesta
+      contenedorHistorial.appendChild(div); // Agrega el nuevo div al contenedor de historial
+    });
+  } catch (error) {
+    console.error('Error al obtener resultados:', error);
+  }
 }
 
-function resetJsonO(objeto) {
-  (objeto.nombre = ""), (objeto.resultado = ""), (color = "");
+async function reiniciarResultados() {
+  try {
+    const response = await fetch('http://localhost:3000/resultados');
+    const resultados = await response.json();
+
+    for (const resultado of resultados) {
+      await fetch(`http://localhost:3000/resultados/${resultado.id}`, { //Se usa el id para eliminar el resultado específico. El id fue seteado en el servidor al crear el resultado por el propio servidor.
+        method: 'DELETE'
+      });
+    }
+
+    mostrarResultados(); // Actualizar la lista de resultados en la interfaz
+    score = 0; // Reiniciar el puntaje
+    document.querySelector("#puntaje").textContent = "Cantidad de respuestas correctas de la sesión: 0";
+
+  } catch (error) {
+    console.error("Error al reiniciar historial:", error);
+  }
 }
 
-main();
+
+
+
+generarPregunta();
+
